1ede1914f4eb5e2a08b68d09c5a4110a2e4ae7ce
Hithaishi Desai <78928904+HithaishiDesai@users.noreply.github.com>
2021-07-13 12:22:05 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,17 +4,17 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>Yo!! <u>{}</u>!! How Are You ??</b>
+<i>I Am A Advance Auto Filter Bot... 😎 By, Hithaishi Desai 😎 @Hithaishidesai_605
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+Add Me To Your Group, Channel And Connect Them And Then You Will Know 😎
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@Hithaishidesai_605
 </i>"""    
     
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
+<b>How To Use Me?</b>
 
 <i>
 -> Add Me To Any Group And Make Me Admin
@@ -43,16 +43,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@Hithaishidesai_605
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> 🤖 Auto Filter Bot 🤖</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Owner</b> : <b><i><a href="https://t.me/Hithaishidesai_605">😎 Hithaishi Desai 😎</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <b><a href="https://github.com/HithaishiDesai/Filter-Bot">Click Me</a></b>
 """
